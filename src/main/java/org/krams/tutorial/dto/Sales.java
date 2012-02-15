4c5bae93523bd2c46fc60997acfba778c3acbae1
Chathuranga Tennakoon <chathuranga.t@gmail.com>
2012-02-15 17:00:41 +0530

    project initiated

---
+++ src/main/java/org/krams/tutorial/dto/Sales.java
@@ -0,0 +1,53 @@
+package org.krams.tutorial.dto;
+
+/**
+ * A simple Data Transfer Object to contain our sales item
+ *
+ */
+public class Sales {
+
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String manufacturer;
+	private String location;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+}
